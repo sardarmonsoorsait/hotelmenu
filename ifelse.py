e07b5054a861f4823f38939e4e08373528beb368
sardarmonsoorsait <sardarmonsoorsait@gmail.com>
2020-06-22 01:51:35 +0530

    problem solved

--- ifelse.py
+++ ifelse.py
@@ -4,4 +4,16 @@ Created on Mon Jun 22 00:33:15 2020
 
 @author: smsait
 """
-
+print ('*******MENU******\n')
+print (' 1 - Biriyani \n 2 - Borrotta \n 3 - Friedrice \n 4 - Mandhi \n')
+item = int(input ('Enter the number for corresponding item  ' ))
+if item == 1:
+    print('your order is Biriyani ,Please wait few minutes')
+elif item == 2:
+    print('your order is Borrotta ,Please wait few minutes')
+elif item == 3:
+    print('your order is friedrice ,Please wait few minutes')
+elif item == 4:
+    print('your order is Mandhi ,Please wait few minutes')
+else:
+    print('you are enter unvalid number ,Please wait few minutes')3
